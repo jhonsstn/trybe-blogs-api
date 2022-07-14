559e5277f698915dc9da9cad9717660c5c82289c
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 16:04:27 -0300

    feat: add getAllUsers route

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const { createUser } = require('../controllers/user-controller');
+const { createUser, getAllUsers } = require('../controllers/user-controller');
 
 const route = Router();
 
 route.post('/', rescue(createUser));
+route.get('/', rescue(getAllUsers));
 
 module.exports = route;
