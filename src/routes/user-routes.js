cd0e31117412c1244c7ad6213c288aa070f47bc0
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 16:33:17 -0300

    feat: add getUserById route

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const { createUser, getAllUsers } = require('../controllers/user-controller');
+const { createUser, getAllUsers, getUserById } = require('../controllers/user-controller');
 
 const route = Router();
 
 route.post('/', rescue(createUser));
 route.get('/', rescue(getAllUsers));
+route.get('/:id', rescue(getUserById));
 
 module.exports = route;
