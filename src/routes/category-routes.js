0015b84c21dc8ff259afbb895170c52eb02b5ed9
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 17:48:45 -0300

    feat: add getAllCategories route

--- src/routes/category-routes.js
+++ src/routes/category-routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const { createCategory } = require('../controllers/category-controller');
+const { createCategory, getAllCategories } = require('../controllers/category-controller');
 
 const route = Router();
 
 route.post('/', rescue(createCategory));
+route.get('/', rescue(getAllCategories));
 
 module.exports = route;
