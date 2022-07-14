d10deaa3d0091b753e42e3c0af4746e6e842781f
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 16:03:54 -0300

    feat: add getAllUsers method to user controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,5 @@
-const { generateToken } = require('../services/auth-service');
-const { createUser, validateUserData } = require('../services/user-service');
+const { generateToken, validateToken } = require('../services/auth-service');
+const { createUser, validateUserData, getAllUsers } = require('../services/user-service');
 
 const UserController = {
   createUser: async (req, res) => {
@@ -7,6 +7,11 @@ const UserController = {
     const user = await createUser(req.body);
     const token = await generateToken(user);
     res.status(201).json({ token });
+  },
+  getAllUsers: async (req, res) => {
+    await validateToken(req.headers.authorization);
+    const users = await getAllUsers();
+    res.status(200).json(users);
   },
 };
 
