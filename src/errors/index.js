6d17681f7b6a51fcab18170f4edfbfb284e0d8e0
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 16:31:10 -0300

    feat: add NotFoundError error

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,9 +1,11 @@
 const BadRequestError = require('./bad-request-error');
 const ConflictError = require('./conflict-error');
 const UnauthorizedError = require('./unauthorized-error');
+const NotFoundError = require('./not-found-error');
 
 module.exports = {
   BadRequestError,
   ConflictError,
   UnauthorizedError,
+  NotFoundError,
 };
