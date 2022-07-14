bf82de3dd77ea1899bacf85cb50712683be10d05
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 17:48:26 -0300

    feat: add getAllCategories method to category controller

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { createCategory } = require('../services/category-service');
+const { createCategory, getAllCategories } = require('../services/category-service');
 const { validateToken } = require('../services/auth-service');
 
 const CategoryController = {
@@ -6,6 +6,11 @@ const CategoryController = {
     await validateToken(req.headers.authorization);
     const newCategory = await createCategory(req.body);
     return res.status(201).json(newCategory);
+  },
+  getAllCategories: async (req, res) => {
+    await validateToken(req.headers.authorization);
+    const categories = await getAllCategories();
+    res.status(200).json(categories);
   },
 };
 
