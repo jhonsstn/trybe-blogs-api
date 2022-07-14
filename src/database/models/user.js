6f6bc10caf9ab5f19e91a007cbd22bcfc566b104
Jhon Santana <jhonsstn@gmail.com>
2022-07-14 17:19:17 -0300

    feat: add User model

---
+++ src/database/models/user.js
@@ -0,0 +1,39 @@
+const { Sequelize } = require('sequelize');
+
+const attributes = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  },
+  displayName: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  email: {
+    unique: true,
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  image: {
+    allowNull: true,
+    type: Sequelize.STRING,
+  },
+}
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', attributes, {
+    timestamps: false,
+  });
+  // User.associate = (models) => {
+  //   User.hasMany(models.BlogPost, {
+  //     foreignKey: 'userId'
+  //   });
+  // }
+  return User;
+}
