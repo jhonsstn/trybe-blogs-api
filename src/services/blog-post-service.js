b185c19600f29e17bdaa10fc29832e87dd1f7671
Jhon Santana <jhonsstn@gmail.com>
2022-07-15 09:22:29 -0300

    feat: add updateBlogPostById method and refactor some variable names in blog post service

--- src/services/blog-post-service.js
+++ src/services/blog-post-service.js
@@ -1,19 +1,19 @@
 const { sequelize } = require('../database/models');
 const { BlogPost, PostCategory, User, Category } = require('../database/models');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, NotFoundError, UnauthorizedError } = require('../errors');
 
 const BlogPostService = {
-  createBlogPost: async (blogPost) => {
-    const blogPostToCreate = {
-      ...blogPost,
+  createBlogPost: async (post) => {
+    const postToCreate = {
+      ...post,
       published: new Date(),
       updated: new Date(),
     };
     return sequelize.transaction(async (t) => {
-      const newBlogPost = await BlogPost.create(blogPostToCreate, { transaction: t });
-      const bulk = blogPost.categoryIds.map((id) => ({ categoryId: id, postId: newBlogPost.id }));
+      const newPost = await BlogPost.create(postToCreate, { transaction: t });
+      const bulk = post.categoryIds.map((id) => ({ categoryId: id, postId: newPost.id }));
       await PostCategory.bulkCreate(bulk, { transaction: t });
-      return newBlogPost;
+      return newPost;
     });
   },
   validateBlogPostData: async (blogPost) => {
@@ -22,14 +22,35 @@ const BlogPostService = {
     if (!title || !content) throw new BadRequestError('Some required fields are missing');
   },
   getAllBlogPosts: async () => {
-    const BlogPosts = await BlogPost.findAll({
+    const posts = await BlogPost.findAll({
       include: [
         { model: User, as: 'user', attributes: { exclude: ['password'] } },
         { model: Category, as: 'categories', through: { attributes: [] } },
       ],
     });
-    console.log(BlogPosts);
-    return BlogPosts;
+    return posts;
+  },
+  getBlogPostById: async (id) => {
+    const post = await BlogPost.findByPk(id, {
+      include: [
+        { model: User, as: 'user', attributes: { exclude: ['password'] } },
+        { model: Category, as: 'categories', through: { attributes: [] } },
+      ],
+    });
+    if (!post) throw new NotFoundError('Post does not exist');
+    return post;
+  },
+  updateBlogPostById: async (id, postData, userId) => {
+    const post = await BlogPost.findByPk(id, {
+      include: [
+        { model: User, as: 'user', attributes: { exclude: ['password'] } },
+        { model: Category, as: 'categories', through: { attributes: [] } },
+      ],
+    });
+    if (!post) throw new NotFoundError('Post does not exist');
+    if (post.userId !== userId) throw new UnauthorizedError('Unauthorized user');
+    await post.update(postData, { where: { id } });
+    return post;
   },
 };
 
