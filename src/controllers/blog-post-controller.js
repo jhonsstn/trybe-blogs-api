f21a0b4e0d1559c7112507cd9bf3841a70f98de9
Jhon Santana <jhonsstn@gmail.com>
2022-07-15 09:22:57 -0300

    feat: add updateBlogPostById method and refactor some variable names in blog post controller

--- src/controllers/blog-post-controller.js
+++ src/controllers/blog-post-controller.js
@@ -1,7 +1,10 @@
 const {
   createBlogPost,
   validateBlogPostData,
-  getAllBlogPosts } = require('../services/blog-post-service');
+  getAllBlogPosts,
+  getBlogPostById,
+  updateBlogPostById,
+} = require('../services/blog-post-service');
 const { validateToken } = require('../services/auth-service');
 const { categoryExists } = require('../services/category-service');
 
@@ -10,13 +13,26 @@ const BlogPostController = {
     const { id: userId } = await validateToken(req.headers.authorization);
     await validateBlogPostData(req.body);
     await Promise.all(req.body.categoryIds.map((categoryId) => categoryExists(categoryId)));
-    const newBlogPost = await createBlogPost({ ...req.body, userId });
-    res.status(201).json(newBlogPost);
+    const newPost = await createBlogPost({ ...req.body, userId });
+    res.status(201).json(newPost);
   },
   getAllBlogPosts: async (req, res) => {
     await validateToken(req.headers.authorization);
-    const blogPosts = await getAllBlogPosts();
-    res.status(200).json(blogPosts);
+    const posts = await getAllBlogPosts();
+    res.status(200).json(posts);
+  },
+  getBlogPostById: async (req, res) => {
+    await validateToken(req.headers.authorization);
+    const { id } = req.params;
+    const post = await getBlogPostById(id);
+    res.status(200).json(post);
+  },
+  updateBlogPostById: async (req, res) => {
+    const { id: userId } = await validateToken(req.headers.authorization);
+    await validateBlogPostData(req.body);
+    const { id } = req.params;
+    const updatedPost = await updateBlogPostById(id, req.body, userId);
+    res.status(200).json(updatedPost);
   },
 };
 
