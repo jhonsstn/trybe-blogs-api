d2147f220c501095c86bf70b6f805fffdfe335fd
Jhon Santana <jhonsstn@gmail.com>
2022-07-15 10:52:07 -0300

    feat: add post category table migration

---
+++ src/database/migrations/20220713200955-add-post-categories-table.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  /**
+   *
+   * @param {import('sequelize')} Sequelize
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories',
+      {
+        postId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          references: {
+            model: 'BlogPosts',
+            key: 'id'
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        },
+        categoryId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          references: {
+            model: 'Categories',
+            key: 'id'
+          },
+          onDelete: 'cascade',
+          onUpdate: 'cascade'
+        }
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
