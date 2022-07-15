ea75c76a608e2a26bcdcc9c6cb43a232b52b1848
Jhon Santana <jhonsstn@gmail.com>
2022-07-15 09:22:43 -0300

    feat: add updateBlogPostById route

--- src/routes/blog-post-routes.js
+++ src/routes/blog-post-routes.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const { createBlogPost, getAllBlogPosts } = require('../controllers/blog-post-controller');
+const {
+  createBlogPost,
+  getAllBlogPosts,
+  getBlogPostById,
+  updateBlogPostById,
+} = require('../controllers/blog-post-controller');
 
 const route = Router();
 
 route.post('/', rescue(createBlogPost));
 route.get('/', rescue(getAllBlogPosts));
+route.get('/:id', rescue(getBlogPostById));
+route.put('/:id', rescue(updateBlogPostById));
 
 module.exports = route;
