910a0b925294a1e2c30c5d8713ef88c39b0b5a1a
Jhon Santana <jhonsstn@gmail.com>
2022-07-15 11:09:25 -0300

    feat: add deleteMe route

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,11 +1,17 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
-const { createUser, getAllUsers, getUserById } = require('../controllers/user-controller');
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  deleteMe,
+} = require('../controllers/user-controller');
 
 const route = Router();
 
 route.post('/', rescue(createUser));
 route.get('/', rescue(getAllUsers));
 route.get('/:id', rescue(getUserById));
+route.delete('/me', rescue(deleteMe));
 
 module.exports = route;
