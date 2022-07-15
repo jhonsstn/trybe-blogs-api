dadc062a2bced4b10467fe12652fa2c9179930af
Jhon Santana <jhonsstn@gmail.com>
2022-07-15 08:46:53 -0300

    feat: add getAllBlogPosts method to blog post controller

--- src/controllers/blog-post-controller.js
+++ src/controllers/blog-post-controller.js
@@ -1,4 +1,7 @@
-const { createBlogPost, validateBlogPostData } = require('../services/blog-post-service');
+const {
+  createBlogPost,
+  validateBlogPostData,
+  getAllBlogPosts } = require('../services/blog-post-service');
 const { validateToken } = require('../services/auth-service');
 const { categoryExists } = require('../services/category-service');
 
@@ -9,6 +12,11 @@ const BlogPostController = {
     await Promise.all(req.body.categoryIds.map((categoryId) => categoryExists(categoryId)));
     const newBlogPost = await createBlogPost({ ...req.body, userId });
     res.status(201).json(newBlogPost);
+  },
+  getAllBlogPosts: async (req, res) => {
+    await validateToken(req.headers.authorization);
+    const blogPosts = await getAllBlogPosts();
+    res.status(200).json(blogPosts);
   },
 };
 
